cff42da30634841714198233be9100a603211f3e
bloges56 <brady.drew.logan@gmail.com>
2020-11-13 13:45:23 -0600

    displays all the places in the console

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,17 @@
 function main() {
   // Put your code here
 
+  //array of all the names
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+  //print out all the places
+  console.log("All Place name")
+
+  for(const name of names){
+    console.log(name)
+  }
+
 }
 
 main();
